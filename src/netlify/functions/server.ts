290ebfb0009b749140fffea096f955f530d8303c
jmoraba_bci <jhan.mora@bci.cl>
2025-03-02 22:26:38 -0300

    add Netlify configuration and serverless function for web scraping

---
+++ src/netlify/functions/server.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import serverless from "serverless-http";
+import { scrapeController } from "../../controllers/scrapeController";
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+
+router.get("/scrape", scrapeController);
+
+app.use("/.netlify/functions/server", router);
+
+module.exports.handler = serverless(app);
